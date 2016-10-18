76486747a8800db2635264e248e52cb11462cb05
Moritz Gruber <moritz@moritzs-macbook-air.local>
2016-10-18 13:09:41 +0200

    created data model

---
+++ pidebugger/pidebugger/raspberrrypi.swift
@@ -0,0 +1,18 @@
+//
+//  raspberrrypi.swift
+//  pidebugger
+//
+//  Created by Moritz Gruber on 18/10/16.
+//  Copyright © 2016 Moritz Gruber. All rights reserved.
+//
+
+import Foundation
+
+var pins = [pin]()
+
+enum pin {
+    case low;
+    case high;
+    case input;
+    
+}
