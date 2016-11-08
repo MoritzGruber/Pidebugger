20def5aa01900a0e61830c652bce39c447b87235
Moritz Gruber <moritz@moritzs-macbook-air.local>
2016-11-08 12:32:06 +0100

    ssh execution works, but socket and ssh service in an new file

--- pidebugger/pidebugger/ViewController.swift
+++ pidebugger/pidebugger/ViewController.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-import SocketIO
-import NMSSH
+
+
 
 class ViewController: UIViewController {
 
@@ -17,37 +17,10 @@ class ViewController: UIViewController {
         
         let pi = raspberrypi.init()
         print("              PI:  \( pi.pins[1])");
-        let socket = SocketIOClient(socketURL: URL(string: "http://10.60.45.94:3000")!, config: [.log(true), .forcePolling(true)])
         
-        socket.on("connect") {data, ack in
-            print("socket connected")
-        }
-        
-        socket.emit("set", 40, 1);
-        
-        socket.connect();
+        socketService.connect();
  
-        let session = NMSSHSession(host: "138.68.74.156", andUsername: "root")
-        session?.connect()
-        if let isConnected = session?.isConnected {
-            print(isConnected)
-            session?.authenticate(byPassword: "asdf1234")
-            /*if let isAuthorized = session?.isAuthorized {
-                print("auth = /(isAuthorized)")
-                
-                do {
-                    try{
-                        print(session?.channel.execute("ls"))
-                    }
-                    
-                } catch {
-                    print("sasfd")
-                }
-                
-            }*/
-        }
-        
-        
+        sshService.start();
         
         
         
---
+++ pidebugger/pidebugger/sshService.swift
@@ -0,0 +1,47 @@
+//
+//  sshService.swift
+//  pidebugger
+//
+//  Created by Moritz Gruber on 08/11/16.
+//  Copyright © 2016 Moritz Gruber. All rights reserved.
+//
+
+import Foundation
+import NMSSH
+
+class sshService {
+    
+   
+    static func start(){
+    let session = NMSSHSession(host: "138.68.74.156", andUsername: "root")
+    session?.connect()
+    if let isConnected = session?.isConnected {
+    print(isConnected)
+    session?.authenticate(byPassword: "********")
+    if let isAuthorized = session?.isAuthorized {
+    print("auth = /(isAuthorized)")
+    do {
+    try print(session?.channel.execute("git clone https://github.com/MoritzGruber/RPI.git"))
+        //install node
+        //pull repo
+        //npm install
+        //run start
+    
+    
+    } catch {
+    print("catchted error")
+    
+    }
+    
+    
+    print("run trough")
+    
+    
+    }
+    }
+
+    }
+    
+}
+
+
