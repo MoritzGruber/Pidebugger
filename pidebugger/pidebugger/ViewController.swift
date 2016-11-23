cd1e8dab3c24083b5ba58cc20a92309a6b34a3c6
MoritzGruber <moritzgruber@yahoo.de>
2016-11-23 14:05:45 +0100

    responsive layout works on iphone se but not on 6plus cause we treat pixels and points the same

--- pidebugger/pidebugger/ViewController.swift
+++ pidebugger/pidebugger/ViewController.swift
@@ -12,6 +12,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var right: UIStackView!
     @IBOutlet weak var left: UIStackView!
     let pi = raspberrypi.init()
     //print("              PI:  \( pi.pins[1])");
@@ -20,7 +21,8 @@ class ViewController: UIViewController {
     //2 variables to temporary store data from the TriStateButton class before adding it to the pinValue Array
     var buttonTag: Int!
     var buttonState: String!
-    
+    var screenwidth : CGFloat!
+    var screenheight : CGFloat!
     
     //Array initialized with 40 default values ("f" = false). Only 26 Pins of the GPIO are user-editable (3 states "f" = false, "t" = true, "i" = input). 14 Pins are non user-editable and will be deactivated ("d" = deactivated).
     var pinValue = [String](repeating: "f", count: 40)
@@ -34,7 +36,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    
+        screenwidth = self.view.frame.size.width
+        screenheight = self.view.frame.size.height
+        print("\(screenwidth) ,\(screenheight)")
         //socketService.connect();
  
         //sshService.start();
@@ -46,20 +50,48 @@ class ViewController: UIViewController {
             currentPin = pin - 1
             pinValue[currentPin] = "d"
         }
+        let screenWidthInPixels = UIScreen.main.nativeBounds.width;
+        let screenHeightInPixels = UIScreen.main.nativeBounds.height;
+        print(screenWidthInPixels);
+        print(screenHeightInPixels);
         
         
+        
+        let spacingTop = 30;
+        let spacingBot = 50;
+        let oneButtonSpace = (Int(screenHeightInPixels) - (spacingTop + spacingBot))/40;
+                print(oneButtonSpace);
+        var yHeight = spacingTop;
+        let xFirstRow = (Int(screenwidth) - 4 * oneButtonSpace);
+        let xSecRow = (Int(screenwidth) - 2 * oneButtonSpace);
+        print(xFirstRow);
+        print(xSecRow);
         // Do any additional setup after loading the view, typically from a nib.
         super.viewDidLoad()
-        for index in 1...40 {
-            let y = index * 30
-            let button = UIButton(frame: CGRect(x: 100, y: y, width: 20, height: 20))
-            button.setTitle("asdfasdf", for: .normal)
-            button.backgroundColor = UIColor.blue
+        for _ in 1...20 {
+            let button = UIButton(frame: CGRect(x: xFirstRow, y: (Int(yHeight)), width: Int(oneButtonSpace)-2, height: Int(oneButtonSpace)-2));
+            button.setTitle("x", for: .normal);
+            button.backgroundColor = UIColor.blue;
             //self.view.addSubview(button)
-            left.addSubview(button)
+            
+                print(yHeight);
+                self.view.addSubview(button);
+            yHeight += Int(oneButtonSpace);
             
         }
-        
+        yHeight = spacingTop;
+        for _ in 1...20 {
+            let button = UIButton(frame: CGRect(x:xSecRow, y: (Int(yHeight)), width: Int(oneButtonSpace)-2, height: Int(oneButtonSpace)-2));
+            button.setTitle("y", for: .normal);
+            button.backgroundColor = UIColor.blue;
+            //self.view.addSubview(button)
+            
+            print(yHeight);
+            self.view.addSubview(button);
+            yHeight += Int(oneButtonSpace);
+            
+        }
+
     }
     
     
