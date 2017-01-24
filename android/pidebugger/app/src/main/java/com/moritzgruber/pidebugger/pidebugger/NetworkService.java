bd43bc7471de852c79e57a6eafff9bf74353d330
mg166 <mg166@hdm-stuttgart.de>
2017-01-24 15:39:48 +0100

    checked comments and typos

--- android/pidebugger/app/src/main/java/com/moritzgruber/pidebugger/pidebugger/NetworkService.java
+++ android/pidebugger/app/src/main/java/com/moritzgruber/pidebugger/pidebugger/NetworkService.java
@@ -12,17 +12,20 @@ import java.net.URL;
  */
 
 public class NetworkService {
+    //get Mac Adress from Ip Adress
     public static String getMacFromArpCache(String ip) {
         if (ip == null)
             return null;
         BufferedReader br = null;
         try {
+            //read buffer
             br = new BufferedReader(new FileReader("/proc/net/arp"));
             String line;
             while ((line = br.readLine()) != null) {
+                //split line
                 String[] splitted = line.split(" +");
                 if (splitted != null && splitted.length >= 4 && ip.equals(splitted[0])) {
-                    // Basic sanity check
+                    //  check format
                     String mac = splitted[3];
                     if (mac.matches("..:..:..:..:..:..")) {
                         return mac;
@@ -45,8 +48,9 @@ public class NetworkService {
         }
         return null;
     }
-
+    //using a api to get the vendor with the mac adress
     public static String getVendor(String urlToRead) throws Exception {
+        //basic http get request
         StringBuilder result = new StringBuilder();
         URL url = new URL(urlToRead);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
